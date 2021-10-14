4f294ae9f0f62894954fbc5e51f656ad95a8f019
vida <ofo28@aol.com>
2021-10-14 17:33:26 -0400

    login form created

--- anywhere-fitness/src/components/Login.jsx
+++ anywhere-fitness/src/components/Login.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
 
 const Login = () => {
-    return ( <div>Login Component</div> );
+    const { login, values, change } = props;
+
+
+    return ( 
+    <div className="loginContainer">
+        <form action="login" onSubmit={}>
+            <h2>Login</h2>
+            <label htmlFor="username"> Username:
+                <input type="type"
+                    name="username"
+                    placeholder="Enter your username"
+                    value={values.username}
+                    onChange={onChange}
+                />    
+            </label>
+            <label htmlFor="password"> <Password></Password>:
+                <input type="type"
+                    name="password"
+                    placeholder="Enter your password"
+                    value={values.password}
+                    onChange={onChange}
+                />    
+            </label>
+
+        </form>
+
+
+    </div> );
 }
  
 export default Login;
