7c282d13816b5d62607c84711bb20c0269798c6f
vida <ofo28@aol.com>
2021-10-16 00:29:15 -0400

    About Components finish

--- anywhere-fitness/src/App.jsx
+++ anywhere-fitness/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Login from './components/Login';
+import About from './components/About';
+
 
 function App() {
   return (
@@ -7,6 +9,9 @@ function App() {
         <nav>
           <h1>Anywhere Fitness</h1>
           <div className="navLinks">
+            <link to= "/about">
+              <button className="about-button">About</button>
+            </link>
             <link to= "/login">
               <button className="login-button">Login</button>
             </link>
@@ -14,7 +19,8 @@ function App() {
           </div>
         </nav>
         <div className="App">
-<Route path="/login" component={Login} />
+            <Route path="/about" component={About} />
+            <Route path="/login" component={Login} />
 
 
         </div>
--- anywhere-fitness/src/components/RegistrationForm.jsx
+++ anywhere-fitness/src/components/RegistrationForm.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
     const RegistrationForm = () => {
-        return ( <div>RegistrationForm Component</div> );
+        return ( 
+        
+        <div>RegistrationForm Component
+
+            <p>
+                <img src ="https://www.bing.com/images/search?view=detailV2&ccid=NJu8aaii&id=BF19C2576B39334F72017ACD222B6B5FD87B9B0C&thid=OIP.NJu8aaiiVNlYaHezd3mK9QHaDt&mediaurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.349bbc69a8a254d9586877b377798af5%3frik%3dDJt72F9rKyLNeg%26riu%3dhttp%253a%252f%252fwww.whslions.net%252flibrary%252fwp-content%252fuploads%252fsites%252f18%252f2014%252f08%252fwelcome-colorful-text-graphic.jpg%26ehk%3dzDPCAETVahrmFEvCVzZewSp36JoMCFnbbbaBZ0AV9yI%253d%26risl%3d%26pid%3dImgRaw%26r%3d0&exph=900&expw=1800&q=welcome+image&simid=608023835755442968&FORM=IRPRST&ck=5E27BE8D17EAC703BDCC661482452B6B&selectedIndex=73" alt="welcome" />
+                
+            </p>
+        </div> );
     }
      
     export default RegistrationForm;
