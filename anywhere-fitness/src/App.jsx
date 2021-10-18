66288a15ac0be1d93d8be3afba86c159c73f134a
Joey Bertschler <joey.bertschler@gmail.com>
2021-10-18 14:19:59 +0200

    adding easteregg game

--- anywhere-fitness/src/App.jsx
+++ anywhere-fitness/src/App.jsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import Login from './components/Login';
+// import React, { useState, useEffect, useContext } from "react";
+// import {Route} from 'react-router-dom';
+// import './App.css'
+// import Login from './components/Login';
+//import Easteregg from './components/Easteregg/easteregg';
+
+import React, {Component} from 'react';
+import EastereggGame from './components/easteregg/EastereggGame'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom' 
+import {Link} from 'react-router-dom'
+
+
 
 function App() {
+
+  const navStyle = {
+    color: 'green',
+    textDecoration: 'none' //anyone knows why this not working?
+  }
+
   return (
+    <Router>
     <div className="App">
-        <nav>
-          <h1>Anywhere Fitness</h1>
+          <h1>Anywhere Fitness (This is App.jsx speaking)</h1>
+          <p> Test </p>
+
+          <nav>
           <div className="navLinks">
-            <link to= "/login">
-              <button className="login-button">Login</button>
-            </link>
-
+            <ul className="header">
+              <Link style={navStyle} to="/login">
+              <li>Login</li>
+              </Link>
+              <Link to="/">
+              <li>Home</li>
+              </Link>
+            </ul>
           </div>
-        </nav>
-        <div className="App">
-<Route path="/login" component={Login} />
+          </nav>
 
+          <Link to='/easteregg' style={{ textDecoration: 'none' }}>
+              Easteregg
+          </Link>
+          <Switch>
+          <Route path="/easteregg" component={EastereggGame}/>
+          </Switch>
 
-        </div>
     </div>
+    </Router>
   );
 }
 
