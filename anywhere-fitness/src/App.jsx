a69e2b5fd9eea272f62117811e865fdd11b098e1
Greg <gbrewton1980@gmail.com>
2021-10-18 22:27:53 -0600

    build(style): styles App.jsx
    creates layout, container

--- anywhere-fitness/src/App.jsx
+++ anywhere-fitness/src/App.jsx
@@ -9,36 +9,49 @@ import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Contact from "./components/Contact";
 import Logout from "./components/Logout";
+import { Header, Container, Divider } from "semantic-ui-react";
 
 function App() {
   return (
-    <div className="App">
-      <header id="header" className="alt">
-        <h1>Anywhere Fitness</h1>
-      </header>
-      <Router>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/dashboard">
-            {/* private route here */}
-            <Dashboard />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/logout">
-            {/* private route here, link should only show if logged in */}
-            <Logout />
-          </Route>
-          <Route path="/">
-            {/* redirect to login if not authenticated, otherwise to dashboard */}
-            <Redirect to="/login" />
-          </Route>
-        </Switch>
-      </Router>
-      <footer><div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></footer>
+    <div id="App">
+      <Header as="h1">Anywhere Fitness</Header>
+      <Container textAlign="center">
+        <Router>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/dashboard">
+              {/* private route here */}
+              <Dashboard />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/logout">
+              {/* private route here, link should only show if logged in */}
+              <Logout />
+            </Route>
+            <Route path="/">
+              {/* redirect to login if not authenticated, otherwise to dashboard */}
+              <Redirect to="/login" />
+            </Route>
+          </Switch>
+        </Router>
+      </Container>
+      <footer>
+        <Divider></Divider>
+        <div>
+          Icons made by{" "}
+          <a href="https://www.freepik.com" title="Freepik">
+            Freepik
+          </a>{" "}
+          from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
