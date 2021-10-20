0777e3d5964c6dde273c20bd3b3abf33d6bfe49c
Greg <gbrewton1980@gmail.com>
2021-10-19 20:32:04 -0600

    chore(wireframes): student login
    UI from login to dashboard with student level authorization

--- anywhere-fitness/src/components/Dashboard.jsx
+++ anywhere-fitness/src/components/Dashboard.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import { Container, Header } from "semantic-ui-react";
+
+// need to tell the difference between instructor and student for conditional rendering
 
 const Dashboard = () => {
-    return ( <div>Dashboard Component</div> );
-}
- 
+    const tempUserName = "Jimmy the Taco Man"
+  return (
+    <Container>
+        <Header as="h2" >Welcome back, {tempUserName}!</Header>
+      {/* greet user */}
+      {/* icon w/badge for "punch card" */}
+      {/* instructor actions (view my classes, create class, edit class, remove class, logout), issue "punch" to a student for completing a class */}
+      {/* student actions (view my classes, register/unregister for a class), browse classes */}
+    </Container>
+  );
+};
+
 export default Dashboard;
