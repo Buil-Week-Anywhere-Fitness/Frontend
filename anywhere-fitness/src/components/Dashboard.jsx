04746ccbe294a207a9de2907c8d96a91e1765755
vida <ofo28@aol.com>
2021-10-20 21:33:07 -0400

    updated

--- anywhere-fitness/src/components/Dashboard.jsx
+++ anywhere-fitness/src/components/Dashboard.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
+import Login from "../components/Login";
+
+import Contact from "../components/Contact";
+import Logout from "../components/Logout";
 const Dashboard = () => {
-    return ( <div>Dashboard Component</div> );
+
+
+    return ( 
+    
+    <div>
+        
+        <nav>
+        <h1>Anywhere Fitness</h1>
+        <div className="nav-links">
+          <Link to="/">
+            <button className="home-button">Home</button>
+          </Link>
+          <Link to="/login">
+            <button className="login-button">Log in</button>
+          </Link>
+          <Link to="/signup">
+            <button className="signup-button">Sign up</button> 
+          </Link>
+          <Link to="/about">
+            <button className="about-button">About</button>
+          </Link>
+          <Link to="/contact">
+            <button className="contact-button">Contact</button>
+          </Link>
+        </div>
+      </nav>
+    </div> );
 }
  
 export default Dashboard;
--- anywhere-fitness/src/components/Login.jsx
+++ anywhere-fitness/src/components/Login.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+
 const Login = () => {
-  return <div>Login Component</div>;
-};
+  const { login, values, change } = props;
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    login();
+  };
+
+  const onChange = (evt) => {
+    const { name, value } = evt.target;
+    change(name, value);
+  };
+
+
+  return ( 
+  <div className="loginContainer">
+      <form action="login" onSubmit={}>
+          <h2>Login</h2>
+          <label htmlFor="username"> Username:
+              <input type="type"
+                  name="username"
+                  placeholder="Enter your username"
+                  value={values.username}
+                  onChange={onChange}
+              />    
+          </label>
+          <label htmlFor="password"> Password:
+              <input type="type"
+                  name="password"
+                  placeholder="Enter your password"
+                  value={values.password}
+                  onChange={onChange}
+              />    
+          </label>
+          <button onClick={handleSubmit}>Log in</button>
+      </form>
+      <p>
+          <img src="./scr/images/fitness.jpg" alt="Time for Fitness"
+          />
+      </p>
+  </div>
+  
+  );
+}
 
 export default Login;
