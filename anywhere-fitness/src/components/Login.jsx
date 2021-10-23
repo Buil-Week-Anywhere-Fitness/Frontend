7efd2707f636c296b3d2dae4f90ff43553f348c2
vida <ofo28@aol.com>
2021-10-22 20:19:22 -0400

    Login About Contact

--- anywhere-fitness/src/components/Login.jsx
+++ anywhere-fitness/src/components/Login.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React from 'react';
 
-const Login = () => {
-  return <div>Login Component</div>;
-};
+export default function Login(props) {
+  const { login, values, change } = props;
 
-export default Login;
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    login();
+  };
+
+  const onChange = (evt) => {
+    const { name, value } = evt.target;
+    change(name, value);
+  };
+
+  return (
+    <div className="loginContainer">
+      <form action="login" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <label htmlFor="username">
+          Username:
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter your username"
+            value={values.username}
+            onChange={onChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password:
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            value={values.password}
+            onChange={onChange}
+          />
+        </label>
+
+        <br />
+        <button onClick={handleSubmit} id="login-button">
+          Login
+        </button>
+      </form>
+      <p>
+        <img
+          src=" ./images/fitness.jpg"
+          alt="time for fitness"
+        />
+      </p>
+    </div>
+  );
+}
