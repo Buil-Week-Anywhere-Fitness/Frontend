9e810eda8efc6cf2c5120d084cedecf5a863f758
Greg Brewton <gbrewton1980@gmail.com>
2021-10-23 16:40:58 -0600

    Revert "Login About Contact"

--- anywhere-fitness/src/components/Login.jsx
+++ anywhere-fitness/src/components/Login.jsx
@@ -1,54 +1,7 @@
-import React from 'react';
+import React from "react";
 
-export default function Login(props) {
-  const { login, values, change } = props;
+const Login = () => {
+  return <div>Login Component</div>;
+};
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    login();
-  };
-
-  const onChange = (evt) => {
-    const { name, value } = evt.target;
-    change(name, value);
-  };
-
-  return (
-    <div className="loginContainer">
-      <form action="login" onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            placeholder="Enter your username"
-            value={values.username}
-            onChange={onChange}
-          />
-        </label>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={values.password}
-            onChange={onChange}
-          />
-        </label>
-
-        <br />
-        <button onClick={handleSubmit} id="login-button">
-          Login
-        </button>
-      </form>
-      <p>
-        <img
-          src=" ./images/fitness.jpg"
-          alt="time for fitness"
-        />
-      </p>
-    </div>
-  );
-}
+export default Login;
