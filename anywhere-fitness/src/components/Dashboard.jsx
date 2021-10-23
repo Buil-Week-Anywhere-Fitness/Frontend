0fc5b1116eb3228ea2b4ff8eda6dd7db44673fef
Greg <gbrewton1980@gmail.com>
2021-10-23 16:18:03 -0600

    Move buttons/header to App-Header, imp. classlist

--- anywhere-fitness/src/components/Dashboard.jsx
+++ anywhere-fitness/src/components/Dashboard.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { Container, Header, Icon } from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
+import ClassList from "./ClassList";
 
 // need to tell the difference between instructor and student for conditional rendering
 
 const Dashboard = () => {
-  const tempUserName = "Jimmy the Taco Man";
   return (
-    <Container>
-      <Header as="h2">
-        <Icon data-testid="user icon" name="user circle outline" size="huge" />
-        Welcome back, {tempUserName}!
-      </Header>
-      {/* greet user */}
-      {/* icon w/badge for "punch card" */}
-      {/* instructor actions (view my classes, create class, edit class, remove class, logout), issue "punch" to a student for completing a class */}
-      {/* student actions (view my classes, register/unregister for a class), browse classes */}
+    <Container fluid>
+      <ClassList />
     </Container>
   );
 };
