bfd12556945a2de04baf538bfff03609942577f0
Greg <gbrewton1980@gmail.com>
2021-10-23 19:55:21 -0600

    feat(logout): confirmation
    clears local storage, redirects

--- anywhere-fitness/src/components/Logout.jsx
+++ anywhere-fitness/src/components/Logout.jsx
@@ -1,12 +1,26 @@
-import React, { useEffect } from "react";
-import { Redirect } from "react-router-dom";
+import React from "react";
+import { Button, Card, Container } from "semantic-ui-react";
+import { useHistory } from "react-router";
 
 const Logout = () => {
-  useEffect(() => {
-    window.localStorage.removeItem("token");
-  });
+  const { push } = useHistory();
+  const logout = () => {
+    window.localStorage.getItem("token") && window.localStorage.removeItem("token");
+    push("/");
+  };
 
-  return <Redirect to="/" />;
+  return (
+    <Container text textAlign="center">
+      <Card fluid>
+        <Card.Header>Are you sure you wish to log out?</Card.Header>
+        <Card.Content>
+          <Button className="ui" basic color="red" onClick={logout}>
+            Yes, I'm sure
+          </Button>
+        </Card.Content>
+      </Card>
+    </Container>
+  );
 };
 
 export default Logout;
